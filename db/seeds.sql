19dda53c45ff6ac59c5eea1cc6841056b330079c
ikkanlaz <andrew.lazenby@gmail.com>
2017-08-09 23:25:58 -0700

    fixing seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers ('name','devoured') VALUES ('Veggie Burger',0);
-INSERT INTO burgers ('name','devoured') VALUES ('Bacon Cheeseburger',0);
-INSERT INTO burgers ('name','devoured') VALUES ('Western Burger',0);
+INSERT INTO burgers (`name`,`devoured`) VALUES ('Veggie Burger',0);
+INSERT INTO burgers (`name`,`devoured`) VALUES ('Bacon Cheeseburger',0);
+INSERT INTO burgers (`name`,`devoured`) VALUES ('Western Burger',0);
